08ba0b0d18d9e497803f1b3e41f4d2a39871bb8b
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2021-11-02 08:32:11 +0100

    Added Write-Output exemption

--- VHDX/tests/general/FileIntegrity.Exceptions.ps1
+++ VHDX/tests/general/FileIntegrity.Exceptions.ps1
@@ -31,7 +31,7 @@ $global:MayContainCommand = @{
 	"Write-Verbose" = @()
 	"Write-Warning" = @()
 	"Write-Error"  = @()
-	"Write-Output" = @()
+	"Write-Output" = @('New-Vhdx.ps1')
 	"Write-Information" = @()
 	"Write-Debug" = @()
 }
